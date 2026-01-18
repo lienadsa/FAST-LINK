33e1be45cc31f3e0c3600349ea800903fed9bcbd
lienadsa <aliyu_daniel@yahoo.com>
2026-01-18 08:30:26 +0100

    database update

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: process.env.FRONTEND_BASE_URL,
   credentials: true
 }));
 
