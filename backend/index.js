caeb104af60903b49e228867470ebd4023da0851
lienadsa <aliyu_daniel@yahoo.com>
2026-01-19 10:54:39 +0100

    finallygit add .!

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ const pool = new Pool({
   database: process.env.PG_DATABASE, 
   password: process.env.PG_PASSWORD, 
   port: process.env.PG_PORT,
-  ssl: { rejectUnauthorized: false },
+  pool_mode: process.env.PG_POOL_MODE
 });
 
 app.use(express.json());
