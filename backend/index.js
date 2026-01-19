8fb466c2bb082dba9ed6904bf48b56fb57a2c81b
lienadsa <aliyu_daniel@yahoo.com>
2026-01-19 16:38:25 +0100

    hopefully now2

--- backend/index.js
+++ backend/index.js
@@ -30,10 +30,27 @@ const pool = new Pool({
 
 
 app.use(cors({
+  origin: function(origin, callback) {
+   
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, origin); 
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization'],
+}));
+
+
+app.options('*', cors({
   origin: allowedOrigins,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization'],
 }));
 
 app.use(express.json());
