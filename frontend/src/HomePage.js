fd9bb8d92e0e337d6024ce66cbbfe7c2f80bac48
lienadsa <aliyu_daniel@yahoo.com>
2026-01-20 13:55:16 +0100

    responsiveness

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -28,11 +28,11 @@ function HomePage () {
       <div className="home">
       <div className="menu"><h1 style={{fontSize: "30px", color: "#F5E7C6", }}>FASTLINK</h1></div>
       <div className="content">
-        <h1 style={{color: "#222222", fontSize: "105px" , fontWeight: "900" ,  marginTop: "100px"}}>THE</h1> 
-        <h1 style={{color: "#222222", fontSize: "105px" , fontWeight: "900"}}>LINKTREE</h1>
-        <h1 style={{color: "#222222", fontSize: "105px" , fontWeight: "900"}}>GENERATOR</h1>
-        <h1 style={{color: "#222222", fontSize: "105px" , fontWeight: "900" }}>MADE FOR</h1>
-        <h1 style={{color: "#222222", fontSize: "105px" , fontWeight: "900"}}>ARTISTS.</h1>
+        <h1 className="maintext"style={{color: "#222222", fontSize: "105px" , fontWeight: "900" ,  marginTop: "100px"}}>THE</h1> 
+        <h1 className="maintext" style={{color: "#222222", fontSize: "105px" , fontWeight: "900"}}>LINKTREE</h1>
+        <h1 className="maintext" style={{color: "#222222", fontSize: "105px" , fontWeight: "900"}}>GENERATOR</h1>
+        <h1 className="maintext" style={{color: "#222222", fontSize: "105px" , fontWeight: "900" }}>MADE FOR</h1>
+        <h1 className="maintext" style={{color: "#222222", fontSize: "105px" , fontWeight: "900"}}>ARTISTS.</h1>
       <div style={{display: "flex" , flexDirection: "row", justifyContent: "space-between" , marginTop: "50px" , marginBottom: "50px"}}><button style={{marginRight: "10px" }} type="button" className="btn btn-outline-dark" name="signup" onClick={Navigation}>Sign Up</button>
       <button style={{marginLeft: "10px"}} type="button" className="btn btn-dark" name="login" onClick={Navigation}>Log in</button></div>
       </div>
